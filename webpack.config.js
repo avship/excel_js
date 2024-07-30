b4da304ffd5728d4bab7d6a3278811bd13e9e51c
avship <alexandershipilo@yandex.ru>
2024-07-30 13:37:25 +0300

    webpack первая сборка

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
